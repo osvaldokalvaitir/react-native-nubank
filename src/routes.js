a0a29aad5943ea0bda9041afad8bc3843a48deae
Osvaldo Kalvaitir Filho <osvaldokalvaitir@outlook.com>
2019-08-17 00:13:03 -0300

    Alteração da cor da statusbar

--- src/routes.js
+++ src/routes.js
@@ -1,7 +1,19 @@
-import { createAppContainer, createSwitchNavigator } from 'react-navigation';
+import { createAppContainer, createSwitchNavigator } from "react-navigation";
 
-import Main from '~/pages/Main';
+import Main from "~/pages/Main";
 
-const Routes = createAppContainer(createSwitchNavigator({ Main }));
+const Routes = createAppContainer(
+  createSwitchNavigator(
+    { Main },
+    {
+      defaultNavigationOptions: {
+        headerStyle: {
+          backgroundColor: "#8b10ae"
+        },
+        headerTintColor: "#ffffff",
+      }
+    }
+  )
+);
 
 export default Routes;
